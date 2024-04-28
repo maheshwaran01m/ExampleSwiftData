45a4cb4bca26f40fbd424c23be135959fa87ee93
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-28 07:09:24 +0530

    Source  - Updated comments

--- ExampleSwiftData/Source/ContentView.swift
+++ ExampleSwiftData/Source/ContentView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
     }
+    .padding()
+  }
 }
 
+// MARK: - Preview
+
 #Preview {
-    ContentView()
+  ContentView()
 }
