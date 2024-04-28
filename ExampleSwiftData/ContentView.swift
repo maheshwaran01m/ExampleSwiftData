9faa4ee0e34465e7646ffe3d0554e5550bc18a6f
MAHESHWARAN <maheshwaran@eam360.com>
2024-04-28 06:55:17 +0530

    Initial Commit

---
+++ ExampleSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleSwiftData
+//
+//  Created by MAHESHWARAN on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleSwiftData/ExampleSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftDataApp.swift
+//  ExampleSwiftData
+//
+//  Created by MAHESHWARAN on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
